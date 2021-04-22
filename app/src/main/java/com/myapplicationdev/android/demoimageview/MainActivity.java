bcd2ea7cff8b7e25d5bc4948ed70536dd67edc40
19022187 <19022187@myrp.edu.sg>
2021-04-22 13:49:28 +0800

    Added codes

--- app/src/main/java/com/myapplicationdev/android/demoimageview/MainActivity.java
+++ app/src/main/java/com/myapplicationdev/android/demoimageview/MainActivity.java
@@ -7,7 +7,7 @@ import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
     // Declare the ImageView object called ivDay2
-    ImageView ivDay2;
+    ImageView ivDay2, ivDay4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +17,12 @@ public class MainActivity extends AppCompatActivity {
         ivDay2 = (ImageView) findViewById(R.id.imageView2);
         // Set the image
         ivDay2.setImageResource(R.drawable.day2);
+
+        // Get the ImageView object and assign to ivDay4
+        ivDay4 = (ImageView) findViewById(R.id.imageView4);
+        // Set the image
+        ivDay4.setImageResource(R.drawable.day4);
+
+
     }
 }
